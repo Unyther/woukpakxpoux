de25a208f669c9e3953eb428c8261d6b7f8cca93
Unyther <91031076+Unyther@users.noreply.github.com>
2025-01-08 11:44:43 +0100

    Delete Rickroll.bat

--- Rickroll.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-powershell -Command ^
-$Shell = New-Object -ComObject WScript.Shell; ^
-for ($i = 0; $i -lt 50; $i++) { $Shell.SendKeys([char]174); Start-Sleep -Milliseconds 50 }; ^
-for ($i = 0; $i -lt 50; $i++) { $Shell.SendKeys([char]175); Start-Sleep -Milliseconds 50 }
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ
